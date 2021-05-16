d22a68be187d8a343283211535ea2de833174ded
wasinaphat-tig <wasinaphat.lil@truedigital.com>
2021-05-16 11:53:08 +0700

    123

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+  res.send(`New Release 0.01 ${target}!`);
 });
 //testes
 const port = process.env.PORT || 8080;
